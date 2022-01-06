51cad62a5af7d6f030ee5e64adb1e6d7e1147a0c
Faruk D <f.diblen@esciencecenter.nl>
2022-01-06 16:08:04 +0100

    added a script and a Dockerfile to build Julia-Python docker images

---
+++ docker/README.md
@@ -0,0 +1,2 @@
+This folder containes a script and a Dockerfile to build
+Docker images with different Python-Julia combinations.
---
+++ docker/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for pyver in 3.10.1 3.9.9
+do
+    for jlver in 1.6.5 1.7.1
+    do
+        echo "Building for Python: ${pyver}  Julia: ${jlver}"
+        tagName=py$(echo ${pyver} | cut -d"." -f1-2)-jl$(echo ${jlver} | cut -d"." -f1-2)
+        docker build --file Dockerfile \
+            --tag ${tagName} \
+            --build-arg PYTHON_VERSION=${pyver} \
+            --build-arg JULIA_VERSION=${jlver} \
+            .
+    done
+done
+
+
